fa40a5f6a246dc5b70eafc4b450bb793a552614c
wanpc3 <wanilhanidrisswanabdulkadir@gmail.com>
2025-01-01 00:45:40 +0800

    Create a slider

--- smartclinic/clinic/static/script.js
+++ smartclinic/clinic/static/script.js
@@ -5,3 +5,44 @@ function onMenuClick() {
 
     navbar.classList.toggle(responsive_class_name);
 }
+
+//For Slider
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0;
+const interval = 3000;
+
+function changeSlide(n) {
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].style.opacity = 0;
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    //Update current image index
+    if (n !== undefined) {
+        currentImg = n;
+    } else {
+        currentImg = (currentImg + 1) % imgs.length;
+    }
+
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+}
+
+//Allow manual slide change
+var timer = setInterval(changeSlide, interval);
+
+//Allow manual slide change
+function manualChange(n) {
+    clearInterval(timer);
+    changeSlide(n);
+    timer = setInterval(changeSlide, interval);
+}
+
+//Attach click handlers for manual changes
+dots.forEach((dot, index) => {
+    dot.onclick = () => manualChange(index);
+});
+
+//Initialize the slider
+changeSlide(0);
